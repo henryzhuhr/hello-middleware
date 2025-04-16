8c5b17ad6cd95b57e8462aa19009bf91e8fdcd2f
henryzhuhr <henry.zhuhr@icloud.com>
2025-04-16 09:53:59 +0800

    🐳 docker: 拆分 docker-compose 文件

---
+++ scripts/docker-compose-up.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker compose \
+    -f docker-compose.yml \
+    -f dockerfiles/docker-compose.es.yml \
+    -f dockerfiles/docker-compose.etcd.yml \
+    -f dockerfiles/docker-compose.mongo.yml \
+    -f dockerfiles/docker-compose.mysql.yml \
+    -f dockerfiles/docker-compose.redis.yml \
+    up --build
