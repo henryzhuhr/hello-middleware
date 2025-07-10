befd5ae2b6fe94472d7db5e5f7adcf777e2d5688
henryzhuhr <henry.zhuhr@icloud.com>
2025-07-10 16:57:01 +0800

    🐳 docker: 更新 Pulsar 集群 Docker Compose 配置
    - 修改 Pulsar 镜像版本为 4.0.0
    - 添加注释

--- go.mod
+++ go.mod
@@ -1,18 +1,12 @@
 module github.com/henryzhuhr/hello-sql
 
-go 1.22.2
+go 1.24
 
-require (
-	github.com/redis/go-redis/v9 v9.7.1
-	github.com/sirupsen/logrus v1.9.3
-)
+require github.com/sirupsen/logrus v1.9.3
 
 require go.uber.org/multierr v1.10.0 // indirect
 
 require (
-	github.com/cespare/xxhash/v2 v2.2.0 // indirect
-	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/google/uuid v1.6.0
 	go.uber.org/zap v1.27.0
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 )
---
+++ scripts/init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Init Python environment
+uv sync --active
+
+# Install Go tools
+export GOPROXY=https://goproxy.cn,direct
+
+go env -w GO111MODULE=on
+go env -w GOPROXY=https://goproxy.cn,direct
+# for Go1.23
+go install golang.org/x/tools/gopls@v0.18.1
+go install github.com/cweill/gotests/...@v1.6.0
+go install github.com/fatih/gomodifytags@v1.17.0
+go install github.com/josharian/impl@v1.4.0
+go install github.com/haya14busa/goplay/cmd/goplay@v1.0.0
+go install github.com/go-delve/delve/cmd/dlv@v1.25.0
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.64.8
+
+# go-zero toolkit
+go install github.com/zeromicro/go-zero/tools/goctl@v1.8.4
+
+go mod tidy
+
